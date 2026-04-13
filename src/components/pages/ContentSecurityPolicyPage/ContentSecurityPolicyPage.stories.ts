035d1af271980335af99475c45b4a1337bd1a653
Cesar Augusto <cesaraugustojun@gmail.com>
2026-04-13 11:53:11 -0300

    feat: implementing iframe page

---
+++ src/components/pages/ContentSecurityPolicyPage/ContentSecurityPolicyPage.stories.ts
@@ -0,0 +1,8 @@
+import ContentSecurityPolicyPage from ".";
+
+export default {
+  title: "components/pages/ContentSecurityPolicy",
+  component: ContentSecurityPolicyPage,
+};
+
+export { ContentSecurityPolicyPage };
---
+++ src/components/pages/ContentSecurityPolicyPage/index.tsx
@@ -0,0 +1,27 @@
+interface IContentSecurityPolicyPageProps
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  > {}
+
+const ContentSecurityPolicyPage = ({
+  children,
+  ...props
+}: IContentSecurityPolicyPageProps) => {
+  return (
+    <iframe
+      src="https://beta.ensinio.com"
+      frameBorder="0"
+      style={{
+        width: "100%",
+        height: "100%",
+        minHeight: "500px",
+        minWidth: "500px",
+      }}
+    >
+      Iframe not supported
+    </iframe>
+  );
+};
+
+export default ContentSecurityPolicyPage;
